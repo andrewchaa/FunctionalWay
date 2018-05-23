72b1e2ecc49b9ae4a1e59573a7b70758692a4c44
Andy <andrew.chaa@yahoo.co.uk>
2018-05-23 20:58:06 +0100

    option class

--- src/FunctionalWay.Tests/OptionTests.cs
+++ src/FunctionalWay.Tests/OptionTests.cs
@@ -1,20 +1,31 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace FunctionalWay.Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void MatchCallsAppropriateFunc()
-        {
-            Assert.Equal("hello, John", Greet(F.Some("John")));
-            Assert.Equal("sorry, who?", Greet(F.None));
-        }
-
         private string Greet(Option<string> name) 
             => name.Match(
                 Some: n => $"hello, {n}",
                 None: () => "sorry, who?");
 
+        [Fact]
+        public void Match_should_call_appropriate_func()
+        {
+            Assert.Equal("hello, John", Greet(F.Some("John")));
+            Assert.Equal("sorry, who?", Greet(F.None));
+        }
+
+        [Fact]
+        public void Should_compare_cointained_value()
+        {
+            Assert.Equal(F.Some(20), F.Some(20));
+        }
+
+
     }
+
+    
 }
--- src/Library/Option.cs
+++ src/Library/Option.cs
@@ -4,71 +4,82 @@ using Unit = System.ValueTuple;
 
 namespace FunctionalWay
 {
-    public static partial class F
+    public static class F
     {
-        public static Option<T> Some<T>(T value) => new Option.Some<T>(value); // wrap the given value into a Some
-        public static Option.None None => Option.None.Default;  // the None value
+        public static Option<T> Some<T>(T value)
+        {
+            return new Some<T>(value);
+        }
+
+        public static None None { get; set; }
     }
 
-    public struct Option<T> : IEquatable<Option.None>, IEquatable<Option<T>>
+    public class Some<T>
     {
-        readonly T _value;
-        readonly bool _isSome;
-        bool IsNone => !_isSome;
+        public T Value { get; }
+        public Some(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class None
+    {
+    }
+
+    public class Option<T> : IEquatable<Option<T>>
+    {
+
+        private readonly T _value;
+        private readonly bool _isSome;
+        private bool IsNone => !_isSome;
 
         private Option(T value)
         {
-            if (value == null)
-                throw new ArgumentNullException();
-            _isSome = true;
             _value = value;
+            _isSome = true;
         }
 
-        public static implicit operator Option<T>(Option.None _) => new Option<T>();
-        public static implicit operator Option<T>(Option.Some<T> some) => new Option<T>(some.Value);
-
-        public static implicit operator Option<T>(T value)
-            => value == null ? F.None : F.Some(value);
-
-        public R Match<R>(Func<R> None, Func<T, R> Some)
-            => _isSome ? Some(_value) : None();
-
-        public IEnumerable<T> AsEnumerable()
+        public Option()
         {
-            if (_isSome) yield return _value;
+            _isSome = false;
+        }
+        
+        public T Match(Func<T> None, Func<T, T> Some)
+        {
+            return _isSome ? Some(_value) : None();
+        }
+        
+        public static implicit operator Option<T>(Some<T> some) => new Option<T>(some.Value); 
+        public static implicit operator Option<T>(None _) => new Option<T>();
+        
+        public bool Equals(Option<T> other)
+        {
+            return _isSome == other._isSome &&
+                   (IsNone || _value.Equals(other._value));
         }
 
-        public bool Equals(Option<T> other) 
-            => this._isSome == other._isSome 
-               && (this.IsNone || this._value.Equals(other._value));
+        public bool Equals(None none) => IsNone;
+        
+        public override bool Equals(object obj)
+        {
+            return Equals((Option<T>) obj);
+        }
 
-        public bool Equals(Option.None _) => IsNone;
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (EqualityComparer<T>.Default.GetHashCode(_value) * 397) ^ _isSome.GetHashCode();
+            }
+        }
+        
 
         public static bool operator ==(Option<T> @this, Option<T> other) => @this.Equals(other);
-        public static bool operator !=(Option<T> @this, Option<T> other) => !(@this == other);
+        public static bool operator !=(Option<T> @this, Option<T> other) => !@this.Equals(other);
 
         public override string ToString() => _isSome ? $"Some({_value})" : "None";
     }
 
-    namespace Option
-    {
-        public struct None
-        {
-            internal static readonly None Default = new None();
-        }
 
-        public struct Some<T>
-        {
-            internal T Value { get; }
-
-            internal Some(T value)
-            {
-                if (value == null)
-                    throw new ArgumentNullException(nameof(value)
-                        , "Cannot wrap a null value in a 'Some'; use 'None' instead");
-                Value = value;
-            }
-        }
-    }
-    
 }
