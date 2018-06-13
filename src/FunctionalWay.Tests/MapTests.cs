9b9fdd4b64f5ec70843cbb3f449146442b9ac243
Andrew Chaa <andrew.yh.chaa@gmail.com>
2018-06-13 15:36:31 +0100

    piping to value tuple

---
+++ src/FunctionalWay.Tests/MapTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using FunctionalWay.Extensions;
+using Xunit;
+
+namespace FunctionalWay.Tests
+{
+    public class MapTests
+    {
+        private Func<(string, string), string> GetFullname =
+            ((string firstName, string lastName) input) => input.firstName + " " + input.lastName;
+
+        [Fact]
+        public void Should_bind_value_tuple()
+        {
+            var fullname = ("Young ho", "Chaa").Map(i => GetFullname(i));
+            
+            Assert.Equal("Young ho Chaa", fullname);
+        }
+    }
+}
